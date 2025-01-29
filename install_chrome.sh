844c80c8c250d9cd7d8654f43181e595074b77a7
caleth28 <114247423+caleth28@users.noreply.github.com>
2025-01-29 14:48:49 -0500

    Update install_chrome.sh

--- install_chrome.sh
+++ install_chrome.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
+# Actualizar el sistema
 apt-get update && apt-get install -y wget unzip
+
+# Instalar Google Chrome
 wget -q -O /tmp/chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i /tmp/chrome.deb || apt-get -fy install
 rm /tmp/chrome.deb
 
-wget -q -O /tmp/chromedriver.zip https://chromedriver.storage.googleapis.com/$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE)/chromedriver_linux64.zip
+# Instalar ChromeDriver compatible con la versión de Chrome instalada
+CHROME_VERSION=$(google-chrome --version | awk '{print $3}' | cut -d '.' -f 1)
+wget -q -O /tmp/chromedriver.zip https://chromedriver.storage.googleapis.com/$(curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION)/chromedriver_linux64.zip
 unzip /tmp/chromedriver.zip -d /usr/local/bin/
 rm /tmp/chromedriver.zip
